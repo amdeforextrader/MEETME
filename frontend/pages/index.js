abe458090dd2fa53257ef4e537892ce4737c25ee
amdeforextrader <amdeforextrader@gmail.com>
2026-04-19 12:02:55 +0100

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold">MeetFriends</h1>
+      <a className="mt-6 text-blue-500 underline" href="/signup">Go to Signup</a>
+    </div>
+  );
+}
